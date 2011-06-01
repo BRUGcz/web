237180ce9d4a6f379ba72067165a42581158e2bc
Marek Jelen <marek@jelen.biz>
2011-06-01 15:15:19 +0200

    Add language mutations, czech language, new member

--- app.rb
+++ app.rb
@@ -10,31 +10,62 @@ module Brug
 
     set :environment, "production"
 
+    enable :sessions
+
+    # ToDo: well, this is not pretty
+    before do
+      if params[:lang]
+        session[:lang] = params[:lang]
+      end
+      unless session[:lang]
+        accept = ["cs", "en"]
+        lang = env["HTTP_ACCEPT_LANGUAGE"]
+        lang = "cs" unless lang
+        lang.split(",").each do |l|
+          if l.include?(";")
+            l = l.split(";")[0]
+          end
+          if accept.include?(l)
+            session[:lang] = l
+            break
+          else
+            accept.each do |a|
+              if l == a or l.match(/^#{a}-/)
+                session[:lang] = a
+                break
+              end
+            end
+            break if session[:lang]
+          end
+        end
+      end
+    end
+
     get '/' do
       @menu = "about"
-      erubis :about
+      erubis "about.#{session[:lang]}".to_sym, :layout => "layout.#{session[:lang]}".to_sym
     end
 
     get '/activities' do
       @menu = "activities"
-      erubis :activities
+      erubis "activities.#{session[:lang]}".to_sym, :layout => "layout.#{session[:lang]}".to_sym
     end
 
     get '/projects' do
       @menu = "projects"
-      erubis :projects
+      erubis "projects.#{session[:lang]}".to_sym, :layout => "layout.#{session[:lang]}".to_sym
     end
 
     get '/members' do
       @menu = "members"
-      erubis :members
+      erubis "members.#{session[:lang]}".to_sym, :layout => "layout.#{session[:lang]}".to_sym
     end
 
     get '/blog' do
       @menu = "blog"
-      erubis :blog
+      erubis "blog.#{session[:lang]}".to_sym, :layout => "layout.#{session[:lang]}".to_sym
     end
-
+    
   end
 
 end
